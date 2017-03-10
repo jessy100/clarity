0a9163f7d58b2cd129d6e5372f3f497db8675303
Dimitry Volker <yerath@Dimitrys-Air.fritz.box>
2017-03-10 15:29:06 +0100

    Changed X into a new variable

--- src/main/java/skadistats/clarity/decoder/Util.java
+++ src/main/java/skadistats/clarity/decoder/Util.java
@@ -11,22 +11,22 @@ import java.lang.reflect.ParameterizedType;
 
 public class Util {
 
-    public static int calcBitsNeededFor(long x) {
-        if (x == 0) return (0);
+    public static int calcBitsNeededFor(long length) {
+        if (length == 0) return (0);
         int n = 32;
-        if (x <= 0x0000FFFF) {
-            n = n - 16; x = x << 16;
+        if (length <= 0x0000FFFF) {
+            n = n - 16; length = length << 16;
         }
-        if (x <= 0x00FFFFFF) {
-            n = n - 8; x = x << 8;
+        if (length <= 0x00FFFFFF) {
+            n = n - 8; length = length << 8;
         }
-        if (x <= 0x0FFFFFFF) {
-            n = n - 4; x = x << 4;
+        if (length <= 0x0FFFFFFF) {
+            n = n - 4; length = length << 4;
         }
-        if (x <= 0x3FFFFFFF) {
-            n = n - 2; x = x << 2;
+        if (length <= 0x3FFFFFFF) {
+            n = n - 2; length = length << 2;
         }
-        if (x <= 0x7FFFFFFF) {
+        if (length <= 0x7FFFFFFF) {
             n = n - 1;
         }
         return n;
